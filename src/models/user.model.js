b0a3792f047953451cf145167fb9eb7bf642273b
ganesh karthik <ganeshkarthik2007.gk@gmail.com>
2026-07-13 15:38:56 +0530

    hash password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,5 +55,15 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+
+
+})
 
 export const User = mongoose.model("User", userSchema);
