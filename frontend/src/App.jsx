5afb478e8300a6696748fb67fdeaedd6df5138a0
ganesh KARTHIK <ganeshkarthik2007.gk@gmail.com>
2026-08-11 03:01:08 +0530

    feat: implement routing for Login, Register, and Home pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
 function App() {
-
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
