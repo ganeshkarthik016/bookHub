ff1433145d3af84f4bcf7afcc81d6581fdb679e3
ganesh KARTHIK <ganeshkarthik2007.gk@gmail.com>
2026-08-20 00:39:48 +0530

    store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice.js";
 import notificationReducer from "./slices/notificationSlice.js";
+import noteReducer from "./slices/noteSlice.js";
+
 
 export const store = configureStore({
     reducer: {
         auth : authReducer,
         notification : notificationReducer,
-        
+        note : noteReducer,
     },
 });
