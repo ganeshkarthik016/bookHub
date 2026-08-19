22c8facdbacddecdd00847b7dacf3b649a4abf72
ganesh KARTHIK <ganeshkarthik2007.gk@gmail.com>
2026-08-19 21:14:55 +0530

    apiError & auth.service.js

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,69 @@
+import api from "./api.js";
+
+import { apiError } from "../utils/apiError.js";
+
+const handleError = (error) => {
+    throw new apiError(
+        error.response?.status || 500,
+        error.response?.data?.message ||
+            "Something went wrong",
+        error.response?.data?.errors || []
+    );
+};
+
+const loginUser = async (credentials) => {
+    try {
+        const response = await api.post(
+            "/users/login",
+            credentials
+        );
+
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+const registerUser = async (userData) => {
+    try {
+        const response = await api.post(
+            "/users/register",
+            userData
+        );
+
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+const logoutUser = async () => {
+    try {
+        const response = await api.post(
+            "/users/logout"
+        );
+
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+const getCurrentUser = async () => {
+    try {
+        const response = await api.get(
+            "/users/current-user"
+        );
+
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export {
+    loginUser,
+    registerUser,
+    logoutUser,
+    getCurrentUser,
+};
