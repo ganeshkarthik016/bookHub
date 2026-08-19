6dd168ae02df709d0ae22d177834aa197b22a468
ganesh KARTHIK <ganeshkarthik2007.gk@gmail.com>
2026-08-19 19:09:44 +0530

    store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-
 function App() {
-
-  return (
-<>
-</>
-  )
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <h1 className="text-4xl font-bold">
+                BookHub
+            </h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
