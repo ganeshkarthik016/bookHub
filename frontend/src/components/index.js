261a0f539562ecdfc242b163acb6f34a04726808
ganesh KARTHIK <ganeshkarthik2007.gk@gmail.com>
2026-08-25 18:02:30 +0530

    index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,6 +2,7 @@
 export { default as Button } from "./ui/Button.jsx";
 export { default as Input } from "./ui/Input.jsx";
 export { default as Modal } from "./ui/Modal.jsx";
+export { default as EmptyState } from "./ui/EmptyState.jsx"
 
 // Cards
 export { default as NoteCard } from "./cards/NoteCard.jsx";
@@ -12,12 +13,23 @@ export { default as NotificationCard } from "./cards/NotificationCard.jsx";
 // Modals
 export { default as UploadNoteModal } from "./modals/UploadNoteModal.jsx";
 export { default as WriteNoteModal } from "./modals/WriteNoteModal.jsx";
+export { default as  CreatePlaylistModal } from "./modals/CreatePlaylistModal.jsx"
 export { default as SharePlaylistModal } from "./modals/SharePlaylistModal.jsx";
+export { default as SaveToPlaylistModal } from "./modals/SaveToPlaylistModal.jsx";
+export { default as ManageMembersModal } from "./modals/ManageMembersModal.jsx";
+export { default as ChangePasswordModal } from "./modals/ChangePasswordModal.jsx";
+export { default as EditProfileModal } from "./modals/EditProfileModal.jsx";
+export { default as DeleteAccountModal } from "./modals/DeleteAccountModal.jsx";
+export {default as OtpModal} from "./modals/OtpModal.jsx";
+export {default as ResetPasswordModal} from "./modals/ResetPasswordModal.jsx";
+
+
 
 // Shared / Interactive
 export { default as FollowButton } from "./shared/FollowButton.jsx";
 export { default as LikeButton } from "./shared/LikeButton.jsx";
 export { default as CommentItem } from "./shared/CommentItem.jsx";
+export {default as CommentSection} from "./shared/CommentSection.jsx";
 
 // Layout
 export { default as MainLayout } from "./layout/MainLayout.jsx";
